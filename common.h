4ef6c3cca11dd84936c68d59a9df2e6daf4e00c0
Damyanka Miteva <damyanka.miteva@mobileinno.com>
2014-02-26 12:41:17 +0200

    resources

---
+++ common.h
@@ -0,0 +1,9 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define WINDOW_ICON ":/images/images/online.png"
+#define ONLINE_ICON ":/images/images/online.png"
+#define OFFLINE_ICON ":/images/images/offline.png"
+
+#endif
+
