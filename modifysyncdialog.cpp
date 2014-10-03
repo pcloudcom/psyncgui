9d26510fdeeb7f0fc8e4afe0c0fa8579f2b923fc
Damyanka Miteva <damyanka.miteva@mobileinno.com>
2014-10-03 17:11:55 +0300

    add tooltips for modifydialg

--- modifysyncdialog.cpp
+++ modifysyncdialog.cpp
@@ -10,7 +10,9 @@ ModifySyncDialog::ModifySyncDialog(QString local, QString remote,int type, QWidg
 {
     ui->setupUi(this);
     ui->label_localVal->setText(local);
-    ui->label_remoteVal->setText(remote);
+    ui->label_localVal->setToolTip(local);
+    ui->label_remoteVal->setText(remote);   
+    ui->label_remoteVal->setToolTip(remote);
     ui->label_DirectionVal->setText(typeNames[type -1]);
     ui->combo_Directions->setCurrentIndex(type-1);
     connect(ui->bntSave, SIGNAL(clicked()), this, SLOT(accept()));
