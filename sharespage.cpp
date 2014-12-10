701ec20b6948f72db5a37c86e54f69c9a6befee1
Damyanka Miteva <damyanka.miteva@mobileinno.com>
2014-12-10 13:15:36 +0200

    SLX-236 | fix links logic for bshares

--- sharespage.cpp
+++ sharespage.cpp
@@ -392,10 +392,10 @@ void SharesPage::acceptRqst()
 
 void SharesPage::openBRqsts()
 {
-    QDesktopServices::openUrl(QUrl(QString("https://mybusiness.pcloud.com/#page=b_shares&sharetab=outshares&authtoken=").append(psync_get_auth_string())));
+    QDesktopServices::openUrl(QUrl(QString("https://mybusiness.pcloud.com/#page=b_shares&sharetab=inshares&authtoken=").append(psync_get_auth_string())));
 }
 
 void SharesPage::openBShares()
 {
-    QDesktopServices::openUrl(QUrl(QString("https://mybusiness.pcloud.com/#page=b_shares&sharetab=inshares&authtoken=").append(psync_get_auth_string())));
+    QDesktopServices::openUrl(QUrl(QString("https://mybusiness.pcloud.com/#page=b_shares&sharetab=outshares&authtoken=").append(psync_get_auth_string())));
 }
